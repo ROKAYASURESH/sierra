db0dc08f357ed7c72e4eefedb4512a6424968abd
SURESHROKAYA <sureshrokaya761@gmail.com>
2023-11-22 17:50:56 +0545

    portfolio Section Add

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -4,7 +4,42 @@ import Common from './Common'
 export default function Portfolio() {
   return (
     <>
-          <Common Name="Portfolio" title="See our best work"/>
+      <Common Name="Portfolio" title="See our best work" />
+      <div class="third-section">
+        <div class="third1">
+          <h2>They are the best</h2>
+          <p>Etiam nec odio vestibulum est mattis effic iturut magna. Pellentesque sit am et tellus blandit.
+            Etiam nec odio vestibul.Cras ex mauris, ornare eget pretium sit amet, dignissim et turpis. Nunc
+            nec maximus dui, vel suscipit dolor. </p>
+          <div class="th">
+            <div class="im">
+              <img src="https://preview.colorlib.com/theme/sierra/img/team/people/people-5.jpg" alt="" />
+            </div>
+            <div class="pr">
+              <p>Chrissa Turner</p>
+              CEO Enterprose
+            </div>
+          </div>
+        </div>
+
+        <div class="third2">
+          <div class="img">
+            <img src="https://preview.colorlib.com/theme/sierra/img/icon/title-icon.png" alt="" />Home
+          </div>
+          <p>DISCOVER THE FEATURES</p>
+          <h2>We don’t hide, we stand tall in front of chalenge</h2>
+          <p class="pr">Etiam nec odio vestibulum est mattis effic iturut magna. Pellentesque sit am et tellus
+            blandit. Etiam nec odio vestibul.Cras ex mauris, ornare eget pretium sit amet, dignissim et
+            turpis. Nunc nec maximus dui, vel suscipit dolor. Donec elementum velit a orci facilisis rutrum.
+            Nam convallis vel erat id dictum. Sed ut risus in orci convallis viverra a eget nisi. Aenean
+            pellentesque elit vitae eros dignissim ultrices.</p>
+        </div>
+
+
+        <div class="third3">
+          <img src="https://preview.colorlib.com/theme/sierra/img/iphone4.png" alt="" />
+        </div>
+      </div>
     </>
   )
 }
