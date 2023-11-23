9ca19e0f20fb8c09271807745e9f43e92479db23
SURESHROKAYA <sureshrokaya761@gmail.com>
2023-11-23 17:09:27 +0545

    Vlog Section

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -5,6 +5,27 @@ export default function Blog() {
   return (
     <>
           <Common Name="Blog" title="Read the news"/>
+      <div class="foot">
+        <div class="service-last">
+            <img src="https://preview.colorlib.com/theme/sierra/img/icon/title-icon.png" alt="" />
+            <h2>DISCOVER THE FEATURES</h2>
+            <h1>Simple solutions for <br></br> complicated times</h1>
+            <p class="pr">Etiam nec odio vestibulum est mattis effic iturut magna.
+              Pellentesque sit am et tellus blandit. Etiam nec odio vestibul.Cras ex mauris,
+              ornare eget pretium sit amet, dignissim et turpis. Nunc nec maximus dui, vel suscipit dolor. Donec
+              elementum velit a orci facilisis rutrum.
+              Nam convallis vel erat id dictum. Sed ut risus in orci convallis viverra a eget nisi. Aenean
+              pellentesque elit vitae eros dignissim ultrices.
+            </p>
+            <ul class="uls">
+              <li class="lis">Cras ex mauris, ornare eget pretium sit amet, dignissim et turpis-</li>
+              <li class="lis">Etiam nec odio vestibulum est mattis effic iturut magna. Pellentesque sit am et
+                tellus blandit.</li>
+              <li class="lis">Sed dapibus nunc vulputate mi fringilla, eu consequat <span>+</span> </li>
+              <li class="bt lis"> Curabitur eu libero sed erat viverra gravida at id ex <span>+</span></li>
+            </ul>
+        </div>
+      </div>
     </>
   )
 }
--- src/BlogDetail.jsx
+++ src/BlogDetail.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function BlogDetail() {
   return (
-    <div>BlogDetail</div>
+    <>
+     
+
+     
+      <div className="lasts">
+        <p>Are you ready to talk?<br></br>
+          contact@sierracompany.com</p>
+      </div>
+    </>
   )
 }
